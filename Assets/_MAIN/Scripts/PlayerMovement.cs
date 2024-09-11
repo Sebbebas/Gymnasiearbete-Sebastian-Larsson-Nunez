197b269c77f1960cdf794819fd4a5b94e82d4622
Sebastian Nuñez Larsson <Sebbebas1005@gmail.com>
2024-09-11 13:08:00 +0200

    Commit 1.1

--- Assets/_MAIN/Scripts/PlayerMovement.cs
+++ Assets/_MAIN/Scripts/PlayerMovement.cs
@@ -1,5 +1,5 @@
+using UnityEditor;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public enum MovementDirection
@@ -17,6 +17,11 @@ public class PlayerMovement : MonoBehaviour
     [Header("Movement")]
     [SerializeField] float movementSpeed = 1.0f;
 
+    [Header("Gizmos")]
+    [SerializeField] LayerMask gizmoLayerMask;
+    [SerializeField] float gizmoOffset;
+    [SerializeField] float gizmoRadius;
+
     //Private Variables
     private Vector3 lastInput;
 
@@ -30,72 +35,105 @@ public class PlayerMovement : MonoBehaviour
         myrigidbody = GetComponent<Rigidbody2D>();
         playerCollider = GetComponent<BoxCollider2D>();
     }
+
     private void Update()
     {
+        Vector2 moveDirection = Vector2.zero;
+
+        switch (lastDirection)
         {
-            //Rotate the player based on the last direction
-            float rotationAngle = 0f;
-            Vector2 moveDirection = Vector2.zero;
-
-            //Determine the angle and the movement direction
-            switch (lastDirection)
-            {
-                case MovementDirection.up:
-                    rotationAngle = 0f;
-                    moveDirection = Vector2.up;
-                    break;
-                case MovementDirection.right:
-                    rotationAngle = -90f;
-                    moveDirection = Vector2.right;
-                    break;
-                case MovementDirection.down:
-                    rotationAngle = 180f;
-                    moveDirection = Vector2.down;
-                    break;
-                case MovementDirection.left:
-                    rotationAngle = 90f;
-                    moveDirection = Vector2.left;
-                    break;
-            }
-
-            //Add rotation
-            myrigidbody.MoveRotation(rotationAngle);
-
-            //Move forward in the direction the player is facing
-            myrigidbody.linearVelocity = moveDirection * movementSpeed;
+            case MovementDirection.up:
+                moveDirection = Vector2.up;
+                break;
+            case MovementDirection.right:
+                moveDirection = Vector2.right;
+                break;
+            case MovementDirection.down:
+                moveDirection = Vector2.down;
+                break;
+            case MovementDirection.left:
+                moveDirection = Vector2.left;
+                break;
         }
+
+        myrigidbody.linearVelocity = moveDirection * movementSpeed;
     }
+
     private void OnEnable()
     {
-        //Get the action map
         var actionMap = inputActions.FindActionMap("Player");
         movementAction = actionMap.FindAction("Move");
-
-        //Enable the action
         movementAction.Enable();
-
-        //Subscribe to the preformed callback
         movementAction.performed += OnMove;
     }
+
     private void OnDisable()
     {
-        //Unsubscribe from the input when the object is disabled
         movementAction.performed -= OnMove;
     }
+
     public void OnMove(InputAction.CallbackContext context)
     {
-        //Get Input Direction
-        Vector2 direction = context.ReadValue<Vector2>();
+        Vector2 overlapDirection = Vector2.zero;
 
-        //Round Input Direction to neareast int
+        switch (lastDirection)
+        {
+            case MovementDirection.up:
+                overlapDirection = Vector2.up;
+                break;
+            case MovementDirection.right:
+                overlapDirection = Vector2.right;
+                break;
+            case MovementDirection.down:
+                overlapDirection = Vector2.down;
+                break;
+            case MovementDirection.left:
+                overlapDirection = Vector2.left;
+                break;
+        }
+
+        if (Physics2D.OverlapCircle((Vector2)transform.position + (overlapDirection * gizmoOffset), gizmoRadius, gizmoLayerMask)) { return; }
+
+        Vector2 direction = context.ReadValue<Vector2>();
         direction.x = Mathf.RoundToInt(direction.x);
         direction.y = Mathf.RoundToInt(direction.y);
 
-        //Get the latest input 
-        if(direction.x != 0 && lastInput.x != direction.x) { if (direction.x > 0) { lastDirection = MovementDirection.right; } else lastDirection = MovementDirection.left; }
-        if(direction.y != 0 && lastInput.y != direction.y) { if (direction.y > 0) { lastDirection = MovementDirection.up; }  else lastDirection = MovementDirection.down; }
+        if (direction.x != 0 && lastInput.x != direction.x)
+        {
+            if (direction.x > 0) { lastDirection = MovementDirection.right; }
+            else { lastDirection = MovementDirection.left; }
+        }
 
-        //Set new lastInput
+        if (direction.y != 0 && lastInput.y != direction.y)
+        {
+            if (direction.y > 0) { lastDirection = MovementDirection.up; }
+            else { lastDirection = MovementDirection.down; }
+        }
+
         lastInput = direction;
+    }
+
+    public void OnDrawGizmosSelected()
+    {
+        Vector2 gizmoDirection = Vector2.zero;
+
+        switch (lastDirection)
+        {
+            case MovementDirection.up:
+                gizmoDirection = Vector2.up;
+                break;
+            case MovementDirection.right:
+                gizmoDirection = Vector2.right;
+                break;
+            case MovementDirection.down:
+                gizmoDirection = Vector2.down;
+                break;
+            case MovementDirection.left:
+                gizmoDirection = Vector2.left;
+                break;
+        }
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere((Vector2)transform.position + (gizmoDirection * gizmoOffset), gizmoRadius);
     }
 }
